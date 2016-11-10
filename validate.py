cb8ac61b1a9dc424a7a9054ff5118c53a25803fe
David Erni <david.erni@4teamwork.ch>
2016-11-10 11:42:55 +0100

    Add configuration example.

--- validate.py
+++ validate.py
@@ -2,11 +2,17 @@ from jsonschema import validate
 from jsonschema import FormatChecker
 import json
 
-with open('schema/content.schema.json') as schema_file:
-    schema = json.load(schema_file)
 
-with open('example-data/content.json') as data_file:
-    data = json.load(data_file)
+def validate_files(filename):
+    with open('schema/{}.schema.json'.format(filename)) as schema_file:
+        schema = json.load(schema_file)
+    with open('example-data/{}.json'.format(filename)) as data_file:
+        data = json.load(data_file)
 
-validate(data, schema, format_checker=FormatChecker())
+    validate(data, schema, format_checker=FormatChecker())
+
+
+validate_files('content')
+validate_files('configuration')
+
 print 'yeah! valid. well done chap!'
