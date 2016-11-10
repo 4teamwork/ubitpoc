03aa905e26a9458aa59d9a99815b00eb8563c7fa
Lukas Graf <lukas.graf@4teamwork.ch>
2016-11-10 09:56:33 +0100

    Fix paths in validate.py after renaming schema/content.json.

--- validate.py
+++ validate.py
@@ -2,10 +2,10 @@ from jsonschema import validate
 from jsonschema import FormatChecker
 import json
 
-with open('schema/repository.schema.json') as schema_file:
+with open('schema/content.schema.json') as schema_file:
     schema = json.load(schema_file)
 
-with open('example-data/repository.json') as data_file:
+with open('example-data/content.json') as data_file:
     data = json.load(data_file)
 
 validate(data, schema, format_checker=FormatChecker())
