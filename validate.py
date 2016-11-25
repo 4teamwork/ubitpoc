3f71b92282cdb8392d828f0e42623003abe686d7
David Erni <dave.erni@gmail.com>
2016-11-25 17:11:45 +0100

    Update example data and schema for flat JSONs. (#2)
    * Update schemas and example-data for flat instead of nested structure.
    
    * Use a "real world" example.

--- validate.py
+++ validate.py
@@ -3,16 +3,34 @@ from jsonschema import FormatChecker
 import json
 
 
-def validate_files(filename):
-    with open('schema/{}.schema.json'.format(filename)) as schema_file:
-        schema = json.load(schema_file)
-    with open('example-data/{}.json'.format(filename)) as data_file:
+OGGBUNDLE_TYPES = (
+    'reporoot',
+    'repofolder',
+    'dossier',
+    'document',
+)
+
+
+def validate_data(document_name, schema_name):
+    doc_fn = 'example-data/%s.json' % document_name
+    with open(doc_fn) as data_file:
         data = json.load(data_file)
 
-    validate(data, schema, format_checker=FormatChecker())
+    schema_fn = 'schema/%s.json' % schema_name
+    with open(schema_fn) as schema_file:
+        schema = json.load(schema_file)
+
+    try:
+        validate(data, schema, format_checker=FormatChecker())
+        print "%s - validation successful" % doc_fn
+    except:
+        print
+        print "%s - validation failed" % doc_fn
+        print
+        raise
 
 
-validate_files('content')
-validate_files('configuration')
+for type_name in OGGBUNDLE_TYPES:
+    validate_data('%ss' % type_name, '%ss.schema' % type_name)
 
-print 'yeah! valid. well done chap!'
+validate_data('configuration', 'configuration.schema')
